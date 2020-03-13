18ca8297f3204797ef5951e2475930d8d2807730
TTamosauskas <thiago.gt@gmail.com>
2020-03-13 11:20:13 -0300

    configuração do multer para upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionControler';
 import ContactController from './app/controllers/ContactController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload - multer(multerConfig;)
 
 routes.post('/users', UserController.store);
 
@@ -18,4 +23,7 @@ routes.put('/contact', ContactController.update);
 
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ok: true })
+})
 export default routes;
