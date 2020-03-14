7a96dbca4b414b0be3611134c18a7d7e4fbdfea2
TTamosauskas <thiago.gt@gmail.com>
2020-03-14 07:25:07 -0300

    listagem de provedores de serviços e avatares

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionControler';
 import ContactController from './app/controllers/ContactController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController'
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,8 @@ routes.post('/contact', ContactController.store);
 routes.put('/contact', ContactController.update);
 
 routes.put('/users', UserController.update);
+
+routes.get('providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
